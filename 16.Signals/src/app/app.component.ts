31a24886bcf492008de89d950eaae7af58a6b933
Zamir Guliyev <zamirquliyev7@gmail.com>
2024-08-28 11:58:20 +0400

    Signals

--- 16.Signals/src/app/app.component.ts
+++ 16.Signals/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
-  template:`
-    <h3>{{count()}}</h3>
+  imports: [RouterOutlet, CommonModule],
+  template: `
+    <h2>{{count()}}</h2>
+    <h3>Computed Count {{countPlus()}}</h3>
+
     <button (click)="increment()">+</button>
     <button (click)="decrement()">-</button>
     <hr>
@@ -21,22 +23,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
-  count = signal(0) 
+  count = signal(0)
   results = signal<string[]>([])
 
+  //computed , effect 
+
+  countPlus = computed(() => this.count() + 1)
+  changeCount = effect(()=> console.log(this.count() + " change"))
+
   //set,update 
-  increment(){
+  increment() {
     //update
-    this.count.update((oldValue)=>oldValue + 1)
+    this.count.update((oldValue) => oldValue + 1)
     //set 
     // this.count.set(0)
     // this.count.set(this.count() + 1)
 
-    this.results.update((oldValue)=>[...oldValue,'increment'])
+    this.results.update((oldValue) => [...oldValue, 'increment'])
   }
 
-  decrement(){
-    this.count.update((oldValue)=>oldValue -1)
-    this.results.update((oldValue)=>[...oldValue,'decrement'])
+  decrement() {
+    this.count.update((oldValue) => oldValue - 1)
+    this.results.update((oldValue) => [...oldValue, 'decrement'])
   }
 }
